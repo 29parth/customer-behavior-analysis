a36e84b4079cec6872931791bf980a6f8d25156d
29parth <parth2004purandare@gmail.com>
2026-06-04 11:12:51 +0530

    Implement customer behavior analysis and database upload

---
+++ customer_behavior_analysis.py
@@ -0,0 +1,71 @@
+import os 
+print(os.getcwd())
+
+import pandas as pd
+df = pd.read_csv('customer_shopping_behavior.csv')
+df.head()
+
+df.isnull().sum()
+
+import pandas as pd
+df = pd.read_csv('customer_shopping_behavior.csv')
+df['Review Rating'] = df.groupby('Category')['Review Rating'].transform(lambda x: x.fillna(x.median()))
+df.isnull().sum()
+
+df.columns = df.columns.str.lower()
+df.columns = df.columns.str.replace(' ','_')
+df = df.rename(columns={'purchase_amount_(usd)':'purchase_amount'})
+
+df.columns
+
+# create a column age_group
+labels = ['Young Adult', 'Adult', 'Middle-aged', 'Senior']
+df['age_group'] = pd.qcut(df['age'], q=4, labels = labels)
+
+df[['age', 'age_group']].head(10)
+
+# create column purchase_frequency_days
+
+frequency_mapping = {
+    'Fortnightly': 14,
+    'Weekly': 7,
+    'Quarterly': 90,
+    'Bi-Weekly': 14,
+    'Annually': 365,
+    'Every 3 Months': 90
+}
+
+df['purchase_frequency_days'] = df['frequency_of_purchases'].map(frequency_mapping)
+
+df[['purchase_frequency_days','frequency_of_purchases']].head(10)
+
+df[['discount_applied','promo_code_used']].head(10)
+
+(df['discount_applied'] == df['promo_code_used']).all()
+
+df = df.drop('promo_code_used', axis=1)
+
+df.columns
+
+pip install psycopg2-binary sqlalchemy
+
+from sqlalchemy import create_engine
+
+username = "postgres"   
+password = "2912%40parth"
+host = "localhost"
+port = "5432"
+database = "customer_behaviour"
+
+engine = create_engine(f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}")
+
+table_name = "customer" 
+df.to_sql(table_name, engine, if_exists="replace", index=False)
+
+print(f"Data succesfully loaded into table '{table_name}' in database '{database}'.")
+
+
+df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+table_name = "customer"
+df.to_sql(table_name, engine, if_exists="replace", index=False)
